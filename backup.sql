333dd4b2b62092427bb5a40ae44d60db0c214615
Eva3Creador <sttefh0@gmail.com>
2025-11-09 02:00:26 -0300

    backup.sql

--- backup.sql
+++ backup.sql
@@ -1,1 +1,7 @@
+-- demo backup
+-- ARCHIVO DE DEMOSTRACIÓN: BORRAR DESPUÉS.
+-- Contenido totalmente ficticio e inocuo.
+CREATE TABLE users (id INT, username TEXT, password TEXT);
+INSERT INTO users VALUES (1,'admin','admin123');
+INSERT INTO users VALUES (2,'demo_user','demo_pass');
 
